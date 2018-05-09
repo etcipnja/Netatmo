7d19deafff0198a6ae2b0bd3ccb3739a37d299d3
etcipnja <eugene@wydevoice.com>
2018-05-09 09:41:01 -0400

    min/max calculation in community mode and better formatting

--- Netatmo/Farmware.py
+++ Netatmo/Farmware.py
@@ -5,6 +5,7 @@ import requests
 class Farmware:
     # ------------------------------------------------------------------------------------------------------------------
     def __init__(self,app_name):
+        self.debug=False
         self.app_name=app_name
         self.api_url = 'https://my.farmbot.io/api/'
         try:
@@ -12,18 +13,26 @@ class Farmware:
         except :
             print("API_TOKEN is not set, you gonna have bad time")
 
-# ------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
     def log(self, message, message_type='info'):
 
-        try:
-            log_message = '[{}] {}'.format(self.app_name, message)
-            node = {'kind': 'send_message', 'args': {'message': log_message, 'message_type': message_type}}
-            response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),headers=self.headers)
-            response.raise_for_status()
-            message = log_message
-        except: pass
+        if not self.debug:
+            try:
+                log_message = '[{}] {}'.format(self.app_name, message)
+                node = {'kind': 'send_message', 'args': {'message': log_message, 'message_type': message_type}}
+                response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),headers=self.headers)
+                response.raise_for_status()
+                message = log_message
+            except: pass
 
         print(message)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def sync(self):
+        if not self.debug:
+            node = {'kind': 'sync', 'args': {}}
+            response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),headers=self.headers)
+            response.raise_for_status()
 
     # ------------------------------------------------------------------------------------------------------------------
     def get(self, enpoint):
@@ -32,24 +41,45 @@ class Farmware:
         return response.json()
 
     # ------------------------------------------------------------------------------------------------------------------
-    def put(self, enpoint, data):
-        response = requests.put(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
-        response.raise_for_status()
-        return response.json()
+    def delete(self, enpoint):
+        if not self.debug:
+            response = requests.delete(self.api_url + enpoint, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+    # ------------------------------------------------------------------------------------------------------------------
+    def post(self, enpoint, data):
+        if not self.debug:
+            response = requests.put(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
 
     # ------------------------------------------------------------------------------------------------------------------
-    def execute_sequence(self, sequence, debug=False, message=''):
-        if sequence['id'] != -1:
+    def put(self, enpoint, data):
+        if not self.debug:
+            response = requests.put(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def patch(self, enpoint, data):
+        if not self.debug:
+            response = requests.patch(self.api_url + enpoint, headers=self.headers, data=json.dumps(data))
+            response.raise_for_status()
+            return response.json()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def execute_sequence(self, sequence, message=''):
+        if sequence != None:
             if message != None:
-                self.log('{}Executing sequence: {}({})'.format(message, sequence['name'], sequence['id']))
-            if not debug:
+                self.log('{}Executing sequence: {}({})'.format(message, sequence['name'].upper(), sequence['id']))
+            if not self.debug:
                 node = {'kind': 'execute', 'args': {'sequence_id': sequence['id']}}
                 response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),
                                          headers=self.headers)
                 response.raise_for_status()
 
     # ------------------------------------------------------------------------------------------------------------------
-    def move_absolute(self, location, offset, debug=False, message=''):
+    def move_absolute(self, location, offset, message=''):
 
         if message!=None:
             self.log('{}Moving absolute: {} {}'.format(message, str(location), "" if offset=={'y': 0, 'x': 0, 'z': 0} else str(offset)))
@@ -62,7 +92,7 @@ class Farmware:
             }
                 }
 
-        if not debug:
+        if not self.debug:
             response = requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=json.dumps(node),
                                      headers=self.headers)
             response.raise_for_status()
